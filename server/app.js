f19842c48748884dd874db852c3919d08b4073a2
kanngji <50470748+kanngji@users.noreply.github.com>
2022-10-17 23:53:57 +0900

    react and backserver

---
+++ server/app.js
@@ -0,0 +1,11 @@
+const express = require("express");
+const app = express();
+
+const PORT = 4000;
+app.use("/", (req, res) => {
+  res.send("asdf");
+});
+
+app.listen(PORT, () => {
+  console.log(`이 서버는 ${PORT}포트에서 실행중입니다`);
+});
